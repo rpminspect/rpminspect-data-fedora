10392bde7f8bb4c0399b1098a19278e95cd0db66
David Cantrell <dcantrell@redhat.com>
2021-01-08 12:18:51 -0500

    Eliminate duplicate entries in licenses/fedora.json
    There were a couple duplicates in here, so eliminate those.  Add a
    script that runs as part of the test step that checks this file for
    duplicate entries.  If any are found, the test step will fail.

---
+++ license_keys_unique.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# figure out where we are and where we want to be
+TOPSRC="$(dirname $(realpath -e $0))"
+LICDIR="${TOPSRC}/licenses"
+
+# missing licenses subdir?  that's an error
+if [ ! -d "${LICDIR}" ]; then
+    echo "*** unable to find licenses subdirectory" >&2
+    exit 1
+fi
+
+# gather license data files (should be one, but you never know)
+cd "${LICDIR}"
+DBFILES="$(ls -1 *.json 2>/dev/null | xargs)"
+
+# no license data files?  that's an error
+if [ -z "${DBFILES}" ]; then
+    echo "*** unable to find any license database files" >&2
+    exit 1
+fi
+
+# check each license data file
+RET=0
+
+for licdb in ${DBFILES} ; do
+    total="$(grep ": {" "${licdb}" | cut -d '"' -f 2 | sort | wc -l)"
+    total_unique="$(grep ": {" "${licdb}" | cut -d '"' -f 2 | sort -u | wc -l)"
+
+    if [ ! "${total}" = "${total_unique}" ]; then
+        echo "*** ${licdb} contains duplicate entries:" >&2
+        grep ": {" "${licdb}" | cut -d '"' -f 2 | sort | uniq -d | sed -e 's|^|*** |g'
+        RET=1
+    fi
+done
+
+exit ${RET}
